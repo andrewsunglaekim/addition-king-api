14a3834425df8bdfc6e297ded3d13a9c3e706796
Andy <andrew.sunglae.kim@gmail.com>
2019-03-30 14:12:04 -0400

    consumes json from front end to create scores

--- Score.js
+++ Score.js
@@ -5,6 +5,15 @@ class Score {
     const scores = await db.any('SELECT * FROM scores;');
     return scores;
   }
+  static async createOne(scoreObject) {
+    await db.none('INSERT INTO scores(userName, numSeconds, numQuestions, numCorrect, answerRange) VALUES($1, $2, $3, $4, $5)', [
+      scoreObject.userName,
+      scoreObject.numSeconds,
+      scoreObject.numQuestions,
+      scoreObject.numCorrect,
+      scoreObject.answerRange,
+    ]);
+  }
 }
 
 module.exports = Score;
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const Score = require('./Score');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
   const scores = await Score.getAll();
   res.json(scores);
+});
+
+app.post('/scores', async(req, res) => {
+  console.log(req.body);
+  try {
+    await Score.createOne(req.body);
+    console.log('created!');
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 
